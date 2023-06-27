0e4b58a6a88d7680e0da8c2960b291475c5c430b
WilgnerFSDev <wilgner.fabio@gmail.com>
2023-06-26 23:34:34 -0300

    Function startup_d2d

--- nvidia-overlay-hijack/src/lib.rs
+++ nvidia-overlay-hijack/src/lib.rs
@@ -1,10 +1,15 @@
 extern crate winapi;
 
+use std::ffi::OsStr;
+use std::os::windows::prelude::OsStrExt;
 use std::ptr::null_mut;
-use winapi::shared::windef::HWND;
-use winapi::um::winuser::{FindWindowA, GetWindowLongA, SetWindowLongPtrA};
-use winapi::um::d2d1::{ID2D1Factory, ID2D1HwndRenderTarget, D2D1CreateFactory, D2D1_FACTORY_TYPE_SINGLE_THREADED, D2D1_RENDER_TARGET_PROPERTIES, D2D1_HWND_RENDER_TARGET_PROPERTIES};
-use winapi::um::dwrite::{IDWriteFactory, DWriteCreateFactory, DWRITE_FACTORY_TYPE_SHARED};
+use winapi::shared::dxgiformat::DXGI_FORMAT_UNKNOWN;
+use winapi::shared::windef::{HWND, RECT};
+use winapi::shared::winerror::S_OK;
+use winapi::um::dcommon::{D2D1_PIXEL_FORMAT, D2D1_ALPHA_MODE_PREMULTIPLIED};
+use winapi::um::winuser::{FindWindowA, GetWindowLongA, SetWindowLongPtrA, GetClientRect};
+use winapi::um::d2d1::{ID2D1Factory, ID2D1HwndRenderTarget, D2D1CreateFactory, D2D1_FACTORY_TYPE_SINGLE_THREADED, D2D1_RENDER_TARGET_PROPERTIES, D2D1_HWND_RENDER_TARGET_PROPERTIES, D2D1_RENDER_TARGET_TYPE_DEFAULT, D2D1_RENDER_TARGET_USAGE_NONE, D2D1_FEATURE_LEVEL_DEFAULT, D2D1_SIZE_U, D2D1_PRESENT_OPTIONS_NONE};
+use winapi::um::dwrite::{IDWriteFactory, DWriteCreateFactory, DWRITE_FACTORY_TYPE_SHARED, IDWriteTextFormat, DWRITE_FONT_WEIGHT_REGULAR, DWRITE_FONT_STYLE_NORMAL, DWRITE_FONT_STRETCH_NORMAL};
 use winapi::um::dwmapi::{DwmExtendFrameIntoClientArea};
 use winapi::um::uxtheme::MARGINS;
 use winapi::Interface;
@@ -20,6 +25,11 @@ pub enum OverlayError {
     FailedToSetWindowPos,
     ShowWindowFailed,
     
+    ID2D1FactoryFailed,
+    StartupD2DFailed,
+    IDWriteFactoryFailed,
+    IDWriteTextFormatFailed,
+
 }
 
 pub struct Overlay {
@@ -27,6 +37,7 @@ pub struct Overlay {
     d2d_factory: Option<ComPtr<ID2D1Factory>>,
     tar: Option<ComPtr<ID2D1HwndRenderTarget>>,
     write_factory: Option<ComPtr<IDWriteFactory>>,
+    format: Option<ComPtr<IDWriteTextFormat>>,
     // ... other fields...
     font: String,
     font_size: f32,
@@ -39,6 +50,7 @@ impl Overlay {
             d2d_factory: None,
             tar: None,
             write_factory: None,
+            format: None,
             
             font: font.to_string(),
             font_size: size,
@@ -91,7 +103,107 @@ impl Overlay {
         Ok(())
     }
 
-    
+    pub fn startup_d2d(&mut self) -> Result<(), OverlayError> {
+        let mut rc: RECT = unsafe { std::mem::zeroed() };
+        
+        let d2d_factory: ComPtr<ID2D1Factory> = unsafe {
+            let mut d2d_factory: *mut ID2D1Factory = std::ptr::null_mut();
+            let hresult = D2D1CreateFactory(
+                D2D1_FACTORY_TYPE_SINGLE_THREADED,
+                &ID2D1Factory::uuidof(),
+                std::ptr::null(),
+                &mut d2d_factory as *mut _ as _,
+            );
+
+            if hresult == S_OK {
+                ComPtr::from_raw(d2d_factory)
+            } else {
+                return Err(OverlayError::ID2D1FactoryFailed);
+            }
+        };
+        
+        let write_factory: ComPtr<IDWriteFactory> = unsafe {
+            let mut write_factory: *mut IDWriteFactory = std::ptr::null_mut();
+            let hresult = DWriteCreateFactory(
+                DWRITE_FACTORY_TYPE_SHARED,
+                &IDWriteFactory::uuidof(),
+                &mut write_factory as *mut _ as _,
+            );
+
+            if hresult == S_OK {
+                ComPtr::from_raw(write_factory)
+            } else {
+                return Err(OverlayError::IDWriteFactoryFailed);
+            }
+        };
+
+        let font_wide: Vec<u16> = OsStr::new(&self.font).encode_wide().chain(Some(0).into_iter()).collect();
+        let locale_name: Vec<u16> = OsStr::new("en-us\0").encode_wide().chain(Some(0).into_iter()).collect();
+        let _format: ComPtr<IDWriteTextFormat> = unsafe {
+            let mut format: *mut IDWriteTextFormat = std::ptr::null_mut();
+            let hresult = (*write_factory).CreateTextFormat(
+                font_wide.as_ptr(),
+                std::ptr::null_mut(),
+                DWRITE_FONT_WEIGHT_REGULAR,
+                DWRITE_FONT_STYLE_NORMAL,
+                DWRITE_FONT_STRETCH_NORMAL,
+                self.font_size,
+                locale_name.as_ptr(),
+                &mut format,
+            );
+
+            if hresult == S_OK {
+                ComPtr::from_raw(format)
+            } else {
+                return Err(OverlayError::IDWriteTextFormatFailed);
+            }
+        };
+        
+        unsafe { GetClientRect(self.window, &mut rc); }
+
+        let tar: ComPtr<ID2D1HwndRenderTarget> = unsafe {
+            let mut tar: *mut ID2D1HwndRenderTarget = std::ptr::null_mut();
+            let render_target_properties = D2D1_RENDER_TARGET_PROPERTIES {
+                _type: D2D1_RENDER_TARGET_TYPE_DEFAULT,
+                pixelFormat: D2D1_PIXEL_FORMAT {
+                    format: DXGI_FORMAT_UNKNOWN,
+                    alphaMode: D2D1_ALPHA_MODE_PREMULTIPLIED,
+                },
+                dpiX: 0.0,
+                dpiY: 0.0,
+                usage: D2D1_RENDER_TARGET_USAGE_NONE,
+                minLevel: D2D1_FEATURE_LEVEL_DEFAULT,
+            };
+
+            let hwnd_target_properties = D2D1_HWND_RENDER_TARGET_PROPERTIES {
+                hwnd: self.window,
+                pixelSize: D2D1_SIZE_U {
+                    width: (rc.right - rc.left) as u32,
+                    height: (rc.bottom - rc.top) as u32,
+                },
+                presentOptions: D2D1_PRESENT_OPTIONS_NONE,
+            };
+
+            let hresult = (*d2d_factory).CreateHwndRenderTarget(
+                &render_target_properties,
+                &hwnd_target_properties,
+                &mut tar,
+            );
+
+            if hresult == S_OK {
+                ComPtr::from_raw(tar)
+            } else {
+                return Err(OverlayError::StartupD2DFailed);
+            }
+        };
+
+        self.d2d_factory = Some(d2d_factory);
+        self.write_factory = Some(write_factory);
+        self.tar = Some(tar);
+
+        Ok(())
+    }
+
 
 }
 
